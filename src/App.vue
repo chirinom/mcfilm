8c860cfe5ecb9f6be34e28ac647e2188319f04c5
MiguelCh <mchirinot@gmail.com>
2023-03-17 17:48:53 -0500

    imageGrid created

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/gallery">Gallery</router-link>
+      <router-link to="/contact">Contact</router-link>
     </nav>
     <router-view />
   </div>
