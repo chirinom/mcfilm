4bffd2467f5e165d29a95b315e03af9d7a4371eb
MiguelCh <mchirinot@gmail.com>
2023-03-18 09:15:25 -0500

    add icons

--- src/components/atoms/ImageGrid.vue
+++ src/components/atoms/ImageGrid.vue
@@ -6,6 +6,9 @@
       class="gallery-grid-item"
     >
       <img :src="require(`@/assets/images/${image}`)" alt="" />
+      <i class="fa-solid fa-shirt shirt"></i>
+      <i class="fa-solid fa-eye eye"></i>
+      <i class="fa-solid fa-circle-info info"></i>
     </div>
   </div>
 </template>
@@ -33,7 +36,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .gallery-grid {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(25%, 1fr));
@@ -43,11 +46,57 @@ export default {
 .gallery-grid-item {
   overflow: hidden;
   height: 555px;
+  transition: opacity 0.2s ease;
+  position: relative;
+
+  &:hover {
+    opacity: 0.6;
+
+    i {
+      display: block;
+    }
+  }
+}
+i {
+  display: none;
+  position: absolute;
+  font-size: 28px;
+  cursor: pointer;
+
+  &:hover {
+    color: lightgrey;
+  }
+}
+
+.shirt {
+  bottom: 33px;
+  left: 22px;
+}
+
+.eye {
+  bottom: 33px;
+  left: 66px;
+}
+
+.info {
+  bottom: 33px;
+  left: 111px;
 }
 
 .gallery-grid-item img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+@media screen and (max-width: 850px) {
+  .gallery-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .gallery-grid {
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 </style>
