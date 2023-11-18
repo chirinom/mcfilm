56a8051f944545d882cffcdaf490ed66d33f6de8
MiguelCh <mchirinot@gmail.com>
2023-11-18 10:05:55 -0500

    lint

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,43 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import ShopView from "../views/ShopView.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ShopView from '../views/ShopView.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
   },
   {
-    path: "/gallery",
-    name: "gallery",
-    component: () => import("../views/GalleryView.vue"),
+    path: '/gallery',
+    name: 'gallery',
+    component: () => import('../views/GalleryView.vue'),
     children: [
       {
-        path: "shop",
+        path: 'shop',
         component: ShopView,
       },
     ],
   },
   {
-    path: "/contact",
-    name: "contact",
-    component: () => import("../views/ContactView.vue"),
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue'),
   },
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
